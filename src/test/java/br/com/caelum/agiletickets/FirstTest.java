ef847521e90ef8862ab3da8e8ca7cc0a1c711a0e
Jose Neto <jose.nogueiraneto@trt14.jus.br>
2014-10-28 13:43:00 -0700

    corrigindo bug na sessao que impedia a reserva igual ao total de ingressos disponiveis

--- src/test/java/br/com/caelum/agiletickets/FirstTest.java
+++ src/test/java/br/com/caelum/agiletickets/FirstTest.java
@@ -1,14 +1,17 @@
 package br.com.caelum.agiletickets;
 
-import static org.junit.Assert.assertTrue;
-
+import org.junit.Assert;
 import org.junit.Test;
+
+import br.com.caelum.agiletickets.models.Sessao;
 
 public class FirstTest {
 
 	@Test
-	public void hello() {
-		assertTrue(true);
+	public void deveVender2IngressosSeHa2Vagas() {
+		Sessao sessao = new Sessao();
+		sessao.setTotalIngressos(2);
+		Assert.assertTrue(sessao.podeReservar(2));
 	}
 	
 }
